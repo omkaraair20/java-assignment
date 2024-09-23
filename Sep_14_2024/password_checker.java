a50d8c06584dca2659c214a40d4ac4e6767b5b9a
omkaraair20 <omkaraair74@gmail.com>
2024-09-23 13:23:47 +0000

    updates

--- Sep_14_2024/password_checker.java
+++ Sep_14_2024/password_checker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Password_Checker {
+public class password_checker {
 
 	public static void main(String[] args) {
 	       Scanner sc=new Scanner(System.in);
